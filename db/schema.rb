552d956aae0c2bb304189aa18d622fee9376963b
N K <konancys@gmail.com>
2014-11-06 22:22:13 -0500

    been awhile since last commit

--- db/schema.rb
+++ db/schema.rb
@@ -11,17 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141029130516) do
+ActiveRecord::Schema.define(version: 20141106031956) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "restaurants", force: true do |t|
-    t.string "name"
-    t.string "address"
-    t.string "telephone"
-    t.text   "description"
-  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -36,9 +29,20 @@ ActiveRecord::Schema.define(version: 20141029130516) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "first_name"
+    t.string   "last_name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+
+  create_table "venues", force: true do |t|
+    t.string   "name"
+    t.string   "address"
+    t.string   "telephone"
+    t.string   "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+		
+
+# require 'ostruct'
+
+url =  "https://api.foursquare.com/v2/venues/search?ll=40.705489,-74.012972&client_id=#{ENV["FOURSQUARE_CLIENT_ID"]}&client_secret=#{ENV["FOURSQUARE_CLIENT_SECRET"]}&v=20141024&&query=coffee"
+
+result = JSON.parse(HTTParty.get(url).body); nil
+
+venues = result["response"]["venues"]; nil
+
+Venue.create(venues.map do |v|
+		{ 
+			name: v["name"],
+			telephone: v["contact"]["formattedPhone"] || "",
+			address: v["location"]["formattedAddress"],
+			description: v["categories"].map { |category| category["name"] }.join(", ")
+		}
+		
+	end)
